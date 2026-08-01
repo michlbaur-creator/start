231dff1e318cdac336ddb35ec9fc66ba011e178d
michlbaur-creator <michlbaur@googlemail.com>
2026-08-01 16:59:28 +0200

    Rueckt iPhone-Kopf und Kacheln weiter nach unten

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Mibaso-Start – Service Worker, network-first (immer aktuell), offline aus dem Cache. */
-const VERSION = 'start-shell-v6';
+const VERSION = 'start-shell-v7';
 const KERN = ['./', './index.html', './manifest.webmanifest', './assets/icon.svg', './assets/icon-192.png'];
 
 self.addEventListener('install', e => {
