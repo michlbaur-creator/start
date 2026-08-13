ac29d9901c83d39427c75b171ec711c451f18196
Michael Baur <mibaur@me.com>
2026-08-13 13:13:11 +0200

    Schriftgroesse im App-Hinweis korrigiert

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Mibaso-Start – Service Worker, network-first (immer aktuell), offline aus dem Cache. */
-const VERSION = 'start-shell-v9';
+const VERSION = 'start-shell-v10';
 const KERN = ['./', './index.html', './ueber/', './impressum/', './manifest.webmanifest', './assets/icon.svg', './assets/icon-192.png'];
 
 self.addEventListener('install', e => {
